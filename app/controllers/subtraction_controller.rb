8250cc7a9c1f8dc73a377dcab6173d7bd97f0856
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-09-26 21:29:55 +0000

    corrected spelling of subtract action & updated parameter spelling

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -3,9 +3,9 @@ class SubtractionController < ApplicationController
     render({ :template => "subtraction_templates/subtraction_form" })
   end
 
-  def subtract_these
+  def subtract
     @first_number = params.fetch("first_num").to_f
-    second_number = params.fetch("second_num").to_f
+    @second_number = params.fetch("second_num").to_f
 
     @result =  @second_number - @first_number
 
